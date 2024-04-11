d60c6f385409a4dd36ea12b4f8118d4d8835deed
JabovichJaba <sergeyfrolov12gmail.com>
2024-04-11 21:52:09 +0300

    LR1_EX1>>Добавлены предикаты childre, mother, brother, b_s.<<

---
+++ LR1/Family_Tree.pl
@@ -0,0 +1,70 @@
+man(voeneg).
+man(ratibor).
+man(boguslav).
+man(velerad).
+man(duhovlad).
+man(svyatoslav).
+man(dobrozhir).
+man(bogomil).
+man(zlatomir).
+
+woman(goluba).
+woman(lubomila).
+woman(bratislava).
+woman(veslava).
+woman(zhdana).
+woman(bozhedara).
+woman(broneslava).
+woman(veselina).
+woman(zdislava).
+
+parent(voeneg,ratibor).
+parent(voeneg,bratislava).
+parent(voeneg,velerad).
+parent(voeneg,zhdana).
+
+parent(goluba,ratibor).
+parent(goluba,bratislava).
+parent(goluba,velerad).
+parent(goluba,zhdana).
+
+parent(ratibor,svyatoslav).
+parent(ratibor,dobrozhir).
+parent(lubomila,svyatoslav).
+parent(lubomila,dobrozhir).
+
+parent(boguslav,bogomil).
+parent(boguslav,bozhedara).
+parent(bratislava,bogomil).
+parent(bratislava,bozhedara).
+
+parent(velerad,broneslava).
+parent(velerad,veselina).
+parent(veslava,broneslava).
+parent(veslava,veselina).
+
+parent(duhovlad,zdislava).
+parent(duhovlad,zlatomir).
+parent(zhdana,zdislava).
+parent(zhdana,zlatomir).
+
+
+men():- man(X), print(X), nl, fail.
+women():- woman(X), print(X), nl, fail.
+% children(X+)
+children(X):- parent(X,Y), print(Y), nl, fail.
+
+% mother(X+, Y+)
+mother(X,Y):- woman(X), parent(X,Y).
+% mother(X+)
+mother(X):- mother(Y,X), print(Y), nl, fail.
+
+% brother(X+, Y+)
+brother(X,Y):- man(X), parent(Z,X), parent(Z,Y), woman(Z), X \= Y.
+% brothers(X+)
+brothers(X):- brother(Y,X), print(Y), nl, fail.
+
+% b_s(X+, Y+)
+b_s(X,Y):- parent(Z,X), parent(Z,Y), woman(Z), X \= Y.
+% b_s(X+)
+b_s(X):- b_s(X, Y), print(Y), nl, fail.
